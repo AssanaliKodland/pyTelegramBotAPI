70c9e5e03aee1fb7d0d6511fc8627808548493f2
AssanaliKodland <saduasan4@gmail.com>
2024-12-15 11:40:26 +0500

    Update chat_join_request.py

--- examples/chat_join_request.py
+++ examples/chat_join_request.py
@@ -9,3 +9,12 @@ def make_some(message: telebot.types.ChatJoinRequest):
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 bot.infinity_polling(allowed_updates=telebot.util.update_types)
+
+def is_api_group(chat_id):
+    return chat_id == GROUP_CHAT_ID
+
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return
